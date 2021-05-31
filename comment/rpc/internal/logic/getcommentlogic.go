cc8ddb0d7284b9beb8be0ed34747357ea0fd649c
heyuan <heyuan@yidian-inc.com>
2021-05-31 21:37:41 +0800

    hy alias ca

---
+++ comment/rpc/internal/logic/getcommentlogic.go
@@ -0,0 +1,30 @@
+package logic
+
+import (
+	"context"
+
+	"zerotest/comment/rpc/comment"
+	"zerotest/comment/rpc/internal/svc"
+
+	"github.com/tal-tech/go-zero/core/logx"
+)
+
+type GetCommentLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewGetCommentLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetCommentLogic {
+	return &GetCommentLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *GetCommentLogic) GetComment(in *comment.IdRequest) (*comment.CommentResponse, error) {
+	// todo: add your logic here and delete this line
+
+	return &comment.CommentResponse{}, nil
+}
